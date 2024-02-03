3c98aa12d61b862508018b5dc22f82d52a6b1f9c
coby_k <cokalter@gmail.com>
2024-02-03 16:09:33 -0500

    Added UI updates to state and fetch request to API as well as a new RecipeExcerpt component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import React from "react";
+import React, { useState, useEffect }  from "react";
 import Header from "./components/Header";
+import RecipeExcerpt from './components/RecipeExcerpt';
 import "./App.css";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  const fetchAllRecipes = async () => {
+      try {
+        const request = await fetch("/api/recipes");
+        const response = await request.json();
+        if (response.ok === false) {
+          console.log("The response is not ok");
+        } else {
+          setRecipes(response);
+        }
+      } catch (error) {
+        console.log("Something went wrong", error);
+      }
+  }
+
+  useEffect(() => {
+    fetchAllRecipes();
+  }, []);
+
   return (
     <div className='recipe-app'>
       <Header />
+      <div className="recipe-list">
+        { recipes.map((recipe) => (
+          <RecipeExcerpt key={recipe.id} recipe={recipe}/>
+          ))}
+      </div>
       <p>Your recipes here! </p>
     </div>
   );
